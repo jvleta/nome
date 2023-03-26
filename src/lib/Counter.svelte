33ca0d0ee7a14dfef74f3ab48492bb7129aa007f
Jimmy Leta <jimmy.leta@gmail.com>
2023-03-26 09:56:01 -0400

    initial commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let count: number = 0
+  const increment = () => {
+    count += 1
+    const click = <HTMLVideoElement> document.getElementById("metronome-beat"); 
+    click.play(); 
+  }
+  const bpm = 80;
+  setInterval(increment, (bpm / 60) * 1000);
+</script>
+
+<audio id="metronome-beat">
+  <source src="/metronome-beat.mp3" type="audio/mpeg">
+</audio>
+
+<button on:click={increment}>
+  count is {count}
+</button>
+
+
